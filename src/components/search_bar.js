e7e920557fe2e5dd480e762daace40eb2057bb85
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-06-20 19:00:24 -0700

    implemented youtube search-bar, video list, item, detail, and css styles

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -15,13 +15,19 @@ class SearchBar extends Component {
     // return <input onChange={this.onInputChange} />;
     // return <input onChange={ event => console.log(event.target.value)} />;
     return (
-      <div>
+      <div className="search-bar">
         <input
         value={this.state.term}
-        onChange={event => this.setState({term: event.target.value})} />
+        onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
   }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
 }
 
 
